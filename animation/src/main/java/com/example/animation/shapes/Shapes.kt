7f99b7f911a26498d333987a081868dfa09d14cb
rishisr22 <rishi.srivastava@moneyview.in>
2024-08-28 22:23:30 +0530

    minor changes

--- animation/src/main/java/com/example/animation/shapes/Shapes.kt
+++ animation/src/main/java/com/example/animation/shapes/Shapes.kt
@@ -1,5 +1,63 @@
 package com.example.animation.shapes
 
-// Ways to draw something on screen
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun RectangleUsingDefaultFunction() {
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        val rectSize = 100.dp.toPx()
+        drawRect(
+            color = Color.White,
+            size = Size(rectSize, rectSize)
+        )
+    }
+}
+
+@Composable
+private fun RectangleUsingPathFunction() {
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        val centerX = size.width / 2
+        val centerY = size.height / 2
+        val path = Path().apply {
+            moveTo(centerX, centerY)
+            lineTo(centerX, centerY + 100.dp.toPx())
+            lineTo(centerX + 100.dp.toPx(), centerY + 100.dp.toPx())
+            lineTo(centerX + 100.dp.toPx(), centerY)
+        }
+        drawPath(
+            path = path,
+            color = Color.White
+        )
+    }
+}
 
 
+@Preview
+@Composable
+fun ShapesPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        RectangleUsingPathFunction()
+    }
+}
